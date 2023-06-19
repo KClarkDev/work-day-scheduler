a7a56bbfbe347255af5e4f9922a86d058f52a2f6
Katherine Clark <60265279+KClarkDev@users.noreply.github.com>
2023-06-19 16:11:39 -0400

    Updated HTML id's to use military time. Added loop in JS file to dynamically apply css to rows based on the current time.

--- Develop/script.js
+++ Develop/script.js
@@ -2,26 +2,37 @@
 
 // References to DOM elements
 var currentDayEl = $("#currentDay");
+var timeBlockEls = $(".time-block"); // returns all elements with this class that we can iterate through
 
 var currentDate = dayjs().format("dddd, MMMM D, YYYY");
-var currentTime = dayjs().format("HH:mm"); // Military time
+var currentHour = parseInt(dayjs().format("HH")); // Military time (string)
 
 $(function () {
-  /* listener evemt for click events on the save button. Uses the id in the containing time-block as a key to save the user input in
-  local storage. */
+  /* listener evemt for click events on the save button. Uses the id in the containing time-block as a key to save the user input in local storage. */
   $("button").click(function () {
-    console.log("You clicked me");
     localStorage.setItem(
       $(this).parent().attr("id"),
-      $(this).siblings(".description").val()
+      $(this).siblings(".description").val() // STUDY NOTE: val() needed instead of text() for inputs
     );
   });
-  //
+
   /* TODO: Add code to apply the past, present, or future class to each time
   block by comparing the id to the current hour. HINTS: How can the id
   attribute of each time-block be used to conditionally add or remove the
-  past, present, and future classes? How can Day.js be used to get the
-  current hour in 24-hour time? */
+  past, present, and future classes?*/
+
+  for (i = 0; i < timeBlockEls.length; i++) {
+    var timeStr = $(timeBlockEls[i]).attr("id").slice(-2);
+    var timeNum = parseInt(timeStr);
+
+    if (currentHour > timeNum) {
+      $(timeBlockEls[i]).addClass("past");
+    } else if (currentHour < timeNum) {
+      $(timeBlockEls[i]).addClass("future");
+    } else {
+      $(timeBlockEls[i]).addClass("present");
+    }
+  }
 
   //
   /* TODO: Add code to get any user input that was saved in localStorage and set
