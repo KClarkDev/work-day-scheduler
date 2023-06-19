0c7ca5a1e15e31efbb237229f686b08a1b573678
Katherine Clark <60265279+KClarkDev@users.noreply.github.com>
2023-06-19 16:28:38 -0400

    Edited comments for clarity

--- Develop/script.js
+++ Develop/script.js
@@ -3,12 +3,13 @@
 // References to DOM elements
 var currentDayEl = $("#currentDay");
 var timeBlockEls = $(".time-block"); // returns all elements with this class that we can iterate through
+var textAreaEls = $(".description");
 
 var currentDate = dayjs().format("dddd, MMMM D, YYYY");
-var currentHour = parseInt(dayjs().format("HH")); // Military time (string)
+var currentHour = parseInt(dayjs().format("HH")); // Military time (converted to int)
 
 $(function () {
-  /* listener evemt for click events on the save button. Uses the id in the containing time-block as a key to save the user input in local storage. */
+  /* Creates listener event for click events on the save button. Uses the id in the containing time-block as a key to save the user input in local storage. */
   $("button").click(function () {
     localStorage.setItem(
       $(this).parent().attr("id"),
@@ -16,10 +17,7 @@ $(function () {
     );
   });
 
-  /* TODO: Add code to apply the past, present, or future class to each time
-  block by comparing the id to the current hour. HINTS: How can the id
-  attribute of each time-block be used to conditionally add or remove the
-  past, present, and future classes?*/
+  /* Applies the past, present, or future class to each time block by comparing the id to the current hour. */
 
   for (i = 0; i < timeBlockEls.length; i++) {
     var timeStr = $(timeBlockEls[i]).attr("id").slice(-2);
@@ -35,9 +33,17 @@ $(function () {
   }
 
   //
-  /* TODO: Add code to get any user input that was saved in localStorage and set
-  the values of the corresponding textarea elements. HINT: How can the id
-  attribute of each time-block be used to do this? */
+  /* Retrieves any user input that was saved in localStorage and sets
+  the values of the corresponding textarea elements. */
+
+  for (i = 0; i < timeBlockEls.length; i++) {
+    var elementID = $(timeBlockEls[i]).attr("id");
+    var rowText = localStorage.getItem(elementID);
+
+    if (rowText !== null) {
+      $(timeBlockEls[i]).children(".description").val(rowText);
+    }
+  }
 
   // Displays the current date in the header of the page.
   currentDayEl.text(currentDate);
